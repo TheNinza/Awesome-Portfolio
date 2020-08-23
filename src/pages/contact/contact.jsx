3ef2879c96dad0bd2d858135613d81bbfaccf36f
TheNinza <niks.a3198@gmail.com>
2020-08-23 11:40:35 +0530

    completed contacts page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -11,6 +11,49 @@ const Contact = () => {
         src={ContactBackground}
         alt="background"
       />
+      <div className="contact-content">
+        <div className="text-field">
+          <div className="impressed">IMPRESSED</div>
+          <div className="question">Want to work with me? Awesome!</div>
+        </div>
+        <div className="contact-me-container">
+          <div className="contact-me">Contact Me</div>
+        </div>
+
+        <div className="contact-icons-container">
+          <div className="contact-icons">
+            <i
+              class="fa fa-facebook-square"
+              aria-hidden="true"
+              onClick={() => window.open("https://www.facebook.com/theninzza")}
+            ></i>
+            <i
+              class="fa fa-linkedin-square"
+              aria-hidden="true"
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/niks-gupta/")
+              }
+            ></i>
+            <i
+              class="fa fa-instagram"
+              aria-hidden="true"
+              onClick={() =>
+                window.open("https://www.instagram.com/nik_gupta._")
+              }
+            ></i>
+            <i
+              class="fa fa-twitter-square"
+              aria-hidden="true"
+              onClick={() => window.open("https://twitter.com/Nik__Gupta")}
+            ></i>
+            <i
+              class="fa fa-github-square"
+              aria-hidden="true"
+              onClick={() => window.open("https://www.github.com/theninza")}
+            ></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
